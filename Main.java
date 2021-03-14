cec6391980f73af844fcb4fa480d9a35bbe3a9fd
Moe Al-Ashtal <58517554+alashtal@users.noreply.github.com>
2021-03-14 19:50:20 +0100

    Created main
    Within the main class there's an object of the controller class that calls all the needed methods.

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  /**
+     * Main to read the string and print out the value
+     * @param args
+     */
+    public static void main(String[] args) {
+				System.out.print("Please enter a name: ");
+				WordsCalculator classObject = new WordsCalculator();
+        String name = classObject.readName();
+
+        System.out.println(classObject.calculator(name));
+
+    }
+}
